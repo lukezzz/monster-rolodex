7ff94a8aa6801fae2bbccaa3d4f64ce750522410
lukezzz <sd65745@126.com>
2020-03-14 22:33:12 +0800

    Updates

---
+++ precache-manifest.ed62e3f45939a78602e1cfb662b544ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304fcf86f498cf15d5f61a7b7f8aec9d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d126a689599107574fc1",
+    "url": "/monsters-rolodex/static/css/main.7d8b17fa.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d126a689599107574fc1",
+    "url": "/monsters-rolodex/static/js/main.e26477d1.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
